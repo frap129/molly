95c23ceca6827fe320db88fc6aaa5350b9e762a4
Sumit Sharma <sumsharma@nvidia.com>
2012-11-08 15:17:45 +0530

    arm: tegra: Added fuse device register common function
    Added fuse platform device registration function
    
    Bug 1238580
    
    Change-Id: I37ea96a1b1030ce1d4bfb25225457865f04fe39e
    Signed-off-by: Sumit Sharma <sumsharma@nvidia.com>
    Reviewed-on: http://git-master/r/162343
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    (cherry picked from commit 223cd8c52911b96755959764188c8d0ca024f12c)
    
    Signed-off-by: Sumit Sharma <sumsharma@nvidia.com>
    Change-Id: Ia22dd9d8bea7dbc1bba11d972a322c85709e9242
    Reviewed-on: http://git-master/r/203575
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -115,6 +115,7 @@ static inline void tegra_clear_framebuffer(unsigned long to, unsigned long size)
 bool is_tegra_debug_uartport_hs(void);
 int get_tegra_uart_debug_port_id(void);
 int arb_lost_recovery(int scl_gpio, int sda_gpio);
+int __init tegra_register_fuse(void);
 
 #define USE_DEFAULT_START_ADDR 0x0
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
