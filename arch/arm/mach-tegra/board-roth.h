7a260521eeb916861b4aecf4d1fab9a7aafe8dff
Xiaohui Tao <xtao@nvidia.com>
2013-03-14 17:52:12 -0700

    ARM: tegra: change orientation to match display for roth
    Roth has a display that is usually 90 degree vertical.
    Change the orientation value to make the sensor behave as
    if it is placed vertical.
    
    Bug 1251119
    
    Change-Id: If69c2b5c144404c8525a72701f36834c88a6cf04
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Reviewed-on: http://git-master/r/209809
    Reviewed-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-roth.h
+++ arch/arm/mach-tegra/board-roth.h
@@ -82,7 +82,7 @@
 #define MPU_GYRO_IRQ_GPIO       TEGRA_GPIO_PR3
 #define MPU_GYRO_ADDR           0x68
 #define MPU_GYRO_BUS_NUM        0
-#define MPU_GYRO_ORIENTATION    { 0, 1, 0, -1, 0, 0, 0, 0, 1 }
+#define MPU_GYRO_ORIENTATION    { 0, 1, 0, 0, 0, 1, 1, 0, 0 }
 #define MPU_COMPASS_NAME        "ak8975"
 #define MPU_COMPASS_IRQ_GPIO    0
 #define MPU_COMPASS_ADDR        0x0C
