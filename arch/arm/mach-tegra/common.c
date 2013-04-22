a79f45c5357191fa8da61c8fefe3a79bef8afadb
Xiaohui Tao <xtao@nvidia.com>
2013-04-22 10:22:59 -0700

    ARM:tegra: Add support to parse touch panel id
    Support to parse touch panel id for kernel
    
    Bug 1253012
    
    Change-Id: I902a1a63efc030cb4b4e82e7301c00027c8e950c
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Reviewed-on: http://git-master/r/221647
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-by: Matt Wagner <mwagner@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -129,6 +129,7 @@ static   bool is_tegra_debug_uart_hsport;
 static struct board_info pmu_board_info;
 static struct board_info display_board_info;
 static int panel_id;
+static int touch_panel_id;
 static struct board_info camera_board_info;
 static struct board_info io_board_info;
 static struct board_info button_board_info;
@@ -889,6 +890,18 @@ static int __init tegra_board_panel_id(char *options)
 	return panel_id;
 }
 __setup("display_panel=", tegra_board_panel_id);
+
+int tegra_get_touch_panel_id(void)
+{
+	return touch_panel_id;
+}
+static int __init tegra_touch_panel_id(char *options)
+{
+	char *p = options;
+	touch_panel_id = memparse(p, &p);
+	return touch_panel_id;
+}
+__setup("touch_panel_id=", tegra_touch_panel_id);
 
 enum power_supply_type get_power_supply_type(void)
 {
