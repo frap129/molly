06f6d58036fdad608683b1f5c8c9f4865166c88d
Ankita Garg <ankitag@nvidia.com>
2013-11-05 14:14:25 -0800

    video: tegra: host: disable gr2d power gate.
    Bug 1300659
    
    Change-Id: I7e18c696ad2616b1696312c1244f52d44b250d85
    Signed-off-by: Ankita Garg <ankitag@nvidia.com>
    Reviewed-on: http://git-master/r/326830
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- drivers/video/tegra/host/t114/t114.c
+++ drivers/video/tegra/host/t114/t114.c
@@ -3,7 +3,7 @@
  *
  * Tegra Graphics Init for Tegra11 Architecture Chips
  *
- * Copyright (c) 2011-2012, NVIDIA Corporation.
+ * Copyright (c) 2011-2013, NVIDIA Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -149,7 +149,7 @@ static struct nvhost_device_data tegra_gr2d03_info = {
 			    {"emc", 300000000, 75 } },
 	.powergate_ids	= { TEGRA_POWERGATE_HEG, -1 },
 	.clockgate_delay = 0,
-	.can_powergate  = true,
+	.can_powergate  = false,
 	.powergate_delay = 100,
 	.powerup_reset	= true,
 	.moduleid	= NVHOST_MODULE_NONE,
