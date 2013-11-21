fff061f5446c978c3fe7d6258aa5d2240f675ae0
Alexandre Courbot <acourbot@nvidia.com>
2013-11-21 17:01:16 +0900

    tegra: dc: don't enable DC if output not connected
    Enabling the DC unconditionally will have some undesired side-effects
    like HDMI signal being emited in the void after boot because the DC
    probe signal and blank ioctl handler will call this function. This patch
    ensures that the DC will not be enabled unless it really should be.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -2168,6 +2168,9 @@ static bool _tegra_dc_enable(struct tegra_dc *dc)
 	if (dc->enabled)
 		return true;
 
+	if (dc->out->type == TEGRA_DC_OUT_HDMI && !tegra_dc_hpd(dc))
+		return false;
+
 	if (!_tegra_dc_controller_enable(dc))
 		return false;
 
