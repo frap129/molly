9e1747a2ac2c82a58a586ad4922c0f0e1eb75e92
Joe Maples <joe@frap129.org>
2017-05-23 13:36:33 -0500

    Revert "video: tegra: dc: hdmi: calling tegra_hotplug_signal to queue deferred work"
    This reverts commit c78a384ef1c59ff480e00d251f9566abdc5d46e4.

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -638,28 +638,6 @@ static int dbg_hotplug_open(struct inode *inode, struct file *file)
 	return single_open(file, dbg_hotplug_show, inode->i_private);
 }
 
-static inline void tegra_hdmi_hotplug_signal(struct tegra_dc_hdmi_data *hdmi)
-{
-	tegra_dc_hpd(hdmi->dc);
-	queue_delayed_work(system_nrt_wq, &hdmi->work,
-		msecs_to_jiffies(30));
-}
-
-static inline void tegra_hdmi_hotplug_disable(struct tegra_dc_hdmi_data *hdmi)
-{
-	struct tegra_dc *dc = hdmi->dc;
-
-	disable_irq(gpio_to_irq(dc->out->hotplug_gpio));
-}
-
-/* enables hotplug IRQ - this must be balanced */
-static inline void tegra_hdmi_hotplug_enable(struct tegra_dc_hdmi_data *hdmi)
-{
-	struct tegra_dc *dc = hdmi->dc;
-
-	enable_irq(gpio_to_irq(dc->out->hotplug_gpio));
-}
-
 static int dbg_hotplug_write(struct file *file, const char __user *addr,
 	size_t len, loff_t *pos)
 {
@@ -678,7 +656,9 @@ static int dbg_hotplug_write(struct file *file, const char __user *addr,
 
 	dc->out->hotplug_state = new_state;
 
-	tegra_hdmi_hotplug_signal(hdmi);
+	queue_delayed_work(system_nrt_wq, &hdmi->work,
+		msecs_to_jiffies(100));
+
 	return len;
 }
 
@@ -973,7 +953,8 @@ static irqreturn_t tegra_dc_hdmi_irq(int irq, void *ptr)
 	spin_lock_irqsave(&hdmi->suspend_lock, flags);
 	if (!hdmi->suspended) {
 		__cancel_delayed_work(&hdmi->work);
-		tegra_hdmi_hotplug_signal(hdmi);
+		queue_delayed_work(system_nrt_wq, &hdmi->work,
+				msecs_to_jiffies(100));
 	}
 	spin_unlock_irqrestore(&hdmi->suspend_lock, flags);
 
@@ -985,9 +966,6 @@ static void tegra_dc_hdmi_suspend(struct tegra_dc *dc)
 	struct tegra_dc_hdmi_data *hdmi = tegra_dc_get_outdata(dc);
 	unsigned long flags;
 
-	/* turn off hotplug detection to avoid resume event when +5V falls */
-	tegra_hdmi_hotplug_disable(hdmi);
-
 	tegra_nvhdcp_suspend(hdmi->nvhdcp);
 	spin_lock_irqsave(&hdmi->suspend_lock, flags);
 	hdmi->suspended = true;
@@ -997,17 +975,17 @@ static void tegra_dc_hdmi_suspend(struct tegra_dc *dc)
 static void tegra_dc_hdmi_resume(struct tegra_dc *dc)
 {
 	struct tegra_dc_hdmi_data *hdmi = tegra_dc_get_outdata(dc);
+	bool hpd = tegra_dc_hdmi_hpd(dc);
 	unsigned long flags;
 
 	spin_lock_irqsave(&hdmi->suspend_lock, flags);
 	hdmi->suspended = false;
 
-	spin_unlock_irqrestore(&hdmi->suspend_lock, flags);
+	queue_delayed_work(system_nrt_wq, &hdmi->work,
+			   msecs_to_jiffies(hpd ? 100 : 30));
 
+	spin_unlock_irqrestore(&hdmi->suspend_lock, flags);
 	tegra_nvhdcp_resume(hdmi->nvhdcp);
-	/* restore hotplug detection */
-	tegra_hdmi_hotplug_enable(hdmi);
-	tegra_hdmi_hotplug_signal(hdmi);
 }
 
 #ifdef CONFIG_SWITCH
