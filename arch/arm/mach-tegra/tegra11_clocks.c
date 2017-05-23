382ffd6bf747324bd738a77ee9d034ff6a9bc998
Joe Maples <joe@frap129.org>
2017-05-23 11:47:08 -0500

    molly: boot.emc: Use SHARED_CLK macro
    Signed-off-by: Joe Maples <joe@frap129.org>

--- arch/arm/mach-tegra/tegra11_clocks.c
+++ arch/arm/mach-tegra/tegra11_clocks.c
@@ -6790,7 +6790,7 @@ struct clk tegra_list_clks[] = {
 	SHARED_CLK("edp.emc",	"edp.emc",		NULL,	&tegra_clk_emc, NULL, 0, SHARED_CEILING),
 	SHARED_CLK("battery.emc", "battery_edp",	"emc",	&tegra_clk_emc, NULL, 0, SHARED_CEILING),
 	SHARED_CLK("floor.profile.emc", "profile.emc", NULL, &tegra_clk_emc, NULL,  0, 0),
-	SHARED_EMC_CLK("boot.emc",	"boot",		"emc",	&tegra_clk_emc, NULL, 0, 0, 0),
+	SHARED_CLK("boot.emc",	"boot",		"emc",	&tegra_clk_emc, NULL, 0, 0),
 
 #ifdef CONFIG_TEGRA_DUAL_CBUS
 	DUAL_CBUS_CLK("3d.cbus",	"tegra_gr3d",		"gr3d",	&tegra_clk_c2bus, "3d",  0, 0),
